cbda4c59fe9a458d80314f6b3c36ebc639dcd280
giggame <46478415+giggame@users.noreply.github.com>
2019-01-08 22:37:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+
+# This is my frist program in PHP
+echo "Hello PHP language!\n";
+
+# Displaying the site name and year
+echo "Marcuscode.com\n";
+echo "2016"
+
+?>
