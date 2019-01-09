333f7977a5f5251b41fde39079e3064299aeacae
giggame <46478415+giggame@users.noreply.github.com>
2019-01-09 11:05:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'ri4Rd+FhJfRW6boOk3eHBUdYSvRa8eG33W3sMb+148WRUl3nAVTxkcCS9LZoNmlDPNdP7FTYSZXLJ0ogmbFrnEiBwsfe+PkRTc3OgmtoxJmRpwTjOgxfYS7gCPXqEA0MsgetfmJfg/jebd9wjof4zQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
