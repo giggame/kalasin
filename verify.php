018a2804b60295e2c87f78fe110e67c278b7f0d8
giggame <46478415+giggame@users.noreply.github.com>
2019-01-09 10:57:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,15 +1,11 @@
 <?php
 $access_token = 'ri4Rd+FhJfRW6boOk3eHBUdYSvRa8eG33W3sMb+148WRUl3nAVTxkcCS9LZoNmlDPNdP7FTYSZXLJ0ogmbFrnEiBwsfe+PkRTc3OgmtoxJmRpwTjOgxfYS7gCPXqEA0MsgetfmJfg/jebd9wjof4zQdB04t89/1O/w1cDnyilFU=';
-
 $url = 'https://api.line.me/v1/oauth/verify';
-
 $headers = array('Authorization: Bearer ' . $access_token);
-
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-
 echo $result;
